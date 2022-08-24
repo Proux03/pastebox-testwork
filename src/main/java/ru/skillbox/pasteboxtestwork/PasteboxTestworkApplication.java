dd213e4d7d38c8c0e4d48054e244bde57ddae87a
proux <proux03@outlook.com>
2022-08-24 19:51:20 +0800

    init

---
+++ src/main/java/ru/skillbox/pasteboxtestwork/PasteboxTestworkApplication.java
@@ -0,0 +1,13 @@
+package ru.skillbox.pasteboxtestwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasteboxTestworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasteboxTestworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/skillbox/pasteboxtestwork/PasteboxTestworkApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skillbox.pasteboxtestwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasteboxTestworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
